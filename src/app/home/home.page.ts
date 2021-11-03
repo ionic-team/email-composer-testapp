35c7490132f698a2fb9c3e635e7a8380463838ea
Chace Daniels <chace@chaces-mbp.lan>
2021-11-03 10:35:20 -0700

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmailComposer } from '@ionic-enterprise/email-composer/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private emailComposer: EmailComposer) {}
+
+  ngOnInit() {
+    this.emailComposer.isAvailable().then(
+      () => { console.log('Email composer available'); },
+      (error) => { console.log('Error, email composer unavailable: ' + error); }
+    );
+  }
+
+  composeButtonClicked() {
+
+    const email = {
+      to: 'max@mustermann.de',
+      cc: 'erika@mustermann.de',
+      bcc: ['john@doe.com', 'jane@doe.com'],
+      attachments: [],
+      subject: 'Cordova Icons',
+      body: 'How are you? Nice greetings from Leipzig',
+    };
+
+    this.emailComposer.open(email);
+  }
 
 }
